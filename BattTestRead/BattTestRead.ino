921d024f8e55505da900dab56fc9fd19cbc50b3e
unknown <tsherburne@tsherburne-01.sumtotalsystems.com>
2013-05-31 07:23:41 -0700

    no message

---
+++ BattTestRead/BattTestRead.ino
@@ -0,0 +1,45 @@
+#include <Wire.h>
+
+#define AT24C256_ADDR 0x50
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+}
+
+boolean done = false;
+void loop(){
+  if(!done) {
+    done = true;
+    readAll(); 
+  }
+}
+
+void readAll() {
+  byte buffer[64];
+
+  Wire.beginTransmission(AT24C256_ADDR);
+  Wire.write(0x00);
+  Wire.write(0x00);
+  Wire.endTransmission();
+
+  for (unsigned short page = 0; page < 3; page++) {
+    Wire.beginTransmission(AT24C256_ADDR);
+    Wire.requestFrom(AT24C256_ADDR, sizeof(buffer));
+    for(int index = 0; index < sizeof(buffer); index++) {
+      buffer[index] = Wire.read();
+    }
+    Wire.endTransmission();
+    
+    for(int index = 0; index < sizeof(buffer); index++) {
+      if (index % 8 == 0) {
+        Serial.println();
+        Serial.print(page * sizeof(buffer) + index, HEX);
+        Serial.print(": ");
+      }
+      Serial.print(buffer[index], HEX);
+      Serial.print(" ");
+    }
+  }
+}
+
---
+++ BattTestWrite/BattTestWrite.ino
@@ -0,0 +1,67 @@
+#include <Wire.h>
+
+#define AT24C256_ADDR 0x50
+#define LED_PIN 13    // LED pin for Pro Mini
+
+void setup() {
+  Wire.begin();
+  Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT);
+}
+
+unsigned short address = 0x0000;
+boolean initDone = false;
+
+void loop() {
+  if (!initDone) {
+    initDone = true;
+    writeLong(address, millis());
+  }
+  
+  digitalWrite(LED_PIN, HIGH);
+  delay(200);
+  digitalWrite(LED_PIN, LOW);
+  delay(200);
+
+  address += 8;
+  writeLong(address, millis());
+}
+
+void writeLong(unsigned short address, unsigned long data) {
+  Serial.print("address = 0x");
+  Serial.println(address, HEX);
+  Serial.print("data = ");
+  Serial.println(data, HEX);
+  
+  byte msb = (address & 0xFF00) >> 8;
+  byte lsb = address & 0x00FF;
+  
+  byte b1 = (data & 0xFF000000) >> 24;
+  byte b2 = (data & 0x00FF0000) >> 16;
+  byte b3 = (data & 0x0000FF00) >> 8;
+  byte b4 = (data & 0x000000FF) >> 0;
+
+  Serial.print("msb/lsb = ");
+  Serial.print(msb, HEX);
+  Serial.println(lsb, HEX);
+  
+  Serial.print("value = 0x");
+  Serial.print(b1, HEX);
+  Serial.print(b2, HEX);
+  Serial.print(b3, HEX);
+  Serial.print(b4, HEX);
+  Serial.println();
+  
+  Wire.beginTransmission(AT24C256_ADDR);
+  Wire.write(msb);
+  Wire.write(lsb);
+  Wire.endTransmission();
+  
+  Wire.beginTransmission(AT24C256_ADDR);
+  Wire.write(b1);
+  Wire.write(b2);
+  Wire.write(b3);
+  Wire.write(b4);
+  Wire.endTransmission();
+}
+
