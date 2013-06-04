2ec8b89a2aff6c096689c052edfb68ccbb799fcc
unknown <tsherburne@tsherburne-01.sumtotalsystems.com>
2013-06-03 22:24:15 -0700

    ReRefactoring into a general-purpose debug tool for the AT24C256

--- AT24c256/AT24c256.ino
+++ AT24c256/AT24c256.ino
@@ -1,19 +1,78 @@
 #include <Wire.h>
 
-#define AT24C256_ADDR 0x50
+#define AT24C256_ADDR   0x50
+#define LED_PIN         13    // LED pin for Pro Mini
+#define EEPROM_KBITS    25
+#define PAGE_BYTES      64
 
 void setup() {
   Serial.begin(9600);
   Wire.begin();
+  pinMode(LED_PIN, OUTPUT);
 }
 
 boolean done = false;
 
 void loop() {
-  if (!done) {
-    done = true;
-    writeBytes();
-    readBytes();
+  prompt();
+}
+
+String readInput() {
+  while(Serial.available() == 0);
+  String result = "";
+  while(Serial.available() > 0) {
+    byte b = Serial.read();
+    if (b == '\n') {break;}
+    else {result += (char) b;}
+  }
+  return result;
+}
+
+void prompt() {
+  printMenu();
+  String input = readInput();
+  int choice = input.toInt();
+  switch(choice) {
+    case 1:
+      promptReadPage();
+    break;
+    
+    case 2:
+      readAll();
+    break;
+    
+    case 3:
+      clearEeprom();
+    break;
+    
+    case 4:
+      testEeprom();
+    break;
+    
+    default:
+      Serial.print("Unknown command ");
+      Serial.println(choice, DEC);
+  } 
+  Serial.flush();
+}
+
+void printMenu() {
+  Serial.println();
+  Serial.println("Atmel EEPROM App");
+  Serial.println("1) read page");
+  Serial.println("2) read all pages");
+  Serial.println("3) erase eeprom");
+  Serial.println("4) test eeprom"); 
+}
+
+void promptReadPage() {
+  Serial.println();
+  Serial.println("Pick a page (1 to 512):");
+  String buffer = readInput();
+  Serial.println(buffer);
+  unsigned short page = buffer.toInt();
+  if (page > 0) {
+    readPage(page - 1);
   }
 }
 
@@ -47,27 +106,36 @@ void writeBytes() {
   Serial.print("rc = "); Serial.println(rc, HEX);
 }
 
-void readBytes() {  
+void readPage(unsigned short page) {
+  page = page * PAGE_BYTES;  
   Wire.beginTransmission(AT24C256_ADDR);
-  Wire.write(0x00);
-  Wire.write(0x08);
+  Wire.write(page >> 8);
+  Wire.write(page & 0x00FF);
   Wire.endTransmission();
 
   long result = 0;
   Wire.beginTransmission(AT24C256_ADDR);
-  Wire.requestFrom(AT24C256_ADDR, 8);
-  for (int i = 0; i < 8; i++) {
+  Wire.requestFrom(AT24C256_ADDR, 64);
+  for (int i = 0; i < 64; i++) {
+    if (i % 8 == 0) {
+      Serial.println();
+      Serial.print((i + page), HEX);
+      Serial.print(":");
+    }
     byte b = Wire.read();
-//    result |= b;
-//    if (i % 4 == 0) {
-//      Serial.println(result, HEX);
-//    }
-//    result = result << 8;
-
     Serial.print(b, HEX);
+    Serial.print(" ");
   }
   Wire.endTransmission();
+  
+  Serial.println();
+}
 
+void readAll() {
+  unsigned short pages = EEPROM_KBITS / 8 * 1024 / PAGE_BYTES;
+  for(unsigned short page = 0; page < pages; page++) {
+     readPage(page);
+  } 
 }
 
 void scan() {
@@ -81,5 +149,36 @@ void scan() {
     }
    }
    Serial.println("scan complete");
+}
+
+void testEeprom() {
+  Serial.println("testing..."); 
+}
+
+/**
+ * Erase the contents of the EEPROM, writing FF into
+ * every byte. Work with 16 bytes at a time to avoid
+ * overflowing the buffer in Wire.h.
+ */
+void clearEeprom() {
+  Serial.println("clearing...");
+  int eepromKbits = 256;
+  int blockBytes = 16;
+  int kbytes = eepromKbits / 8 * 1024;
+  int blocks = kbytes / blockBytes;
+  
+  for (int block = 0; block < blocks; block++) {
+    unsigned short address = block * blockBytes;
+    Wire.beginTransmission(AT24C256_ADDR);
+    Wire.write(address >> 8);
+    Wire.write(address & 0x00FF);
+    for (int index = 0; index < blockBytes; index++) {
+      Wire.write(0xFF);
+    }
+    Wire.endTransmission();
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.println("...done");
 }
 
