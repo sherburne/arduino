9cce94edd58f0f76bab75628d647fc942ad01f88
sherb <sherb@creature.local>
2013-06-09 13:11:17 -0700

    Added additional supporting libs for LiquidCrystal_I2C, fixed the constructor bits

--- LCD1602Sample/LCD1602Sample.ino
+++ LCD1602Sample/LCD1602Sample.ino
@@ -1,15 +1,36 @@
 #include<Wire.h>
-#include<LCD.h>
-#include<LiquidCrystal_I2C.h>
+#include"LiquidCrystal_I2C.h"
 
 #define LCD_TWI_ADDR 0x20
 
+// If you're not sure how to get your LCD and backpack to work, check out these threads:
+// http://forum.arduino.cc/index.php?topic=142255.0
+// http://forum.arduino.cc/index.php?topic=157817.0
+//
+// Data sheet for the red LCD:
+// http://www.geeetech.com/wiki/index.php/1602_LCD
+//
+// Library for the LCD
+// https://bitbucket.org/fmalpartida/new-liquidcrystal/overview
+
+// LCD connects are: VSS, VDD, VO, RS, RW, E, D0, D1, D2, D3, D4, D5, D6, D7, A (or LED+), K (or LED-)
+LiquidCrystal_I2C lcd(LCD_TWI_ADDR, 4, 5, 6, 0, 1, 2, 3, 7, NEGATIVE);
+
 void setup() {
-  Wire.begin();
-  LiquidCrystal_I2C.begin(20, 2, LCD.LCD_5x8DOTS);
+  lcd.begin(16, 2, LCD_5x8DOTS);
 }
 
-void loop() {
-  
-  
+boolean done = false;
+void loop()
+{
+  if(!done) {
+    lcd.backlight();
+    lcd.clear();
+    lcd.home();
+    lcd.print("ABCDEFGHIJKLM");
+    lcd.setCursor(0, 2);
+    lcd.print("NOPQRSTUVWXYZ");
+    done = true;
+  }
 }
+
